4b253cea49938610db6f484161537549afc2b9d8
ChristianChenet <christian@controlsconsultoria.com.br>
2026-09-08 16:16:24 -0300

    Alinha status contabilidade no grid de entradas

--- app/templates/layout_top.php
+++ app/templates/layout_top.php
@@ -6,7 +6,7 @@
     <title><?= h($title) ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="<?= h(base_url('assets/logo-s-novo.jpg')) ?>">
-    <link rel="stylesheet" href="<?= h(base_url('assets/app.css?v=20260908-documents-tomador')) ?>">
+    <link rel="stylesheet" href="<?= h(base_url('assets/app.css?v=20260908-accounting-row-align')) ?>">
 </head>
 <body>
 <div class="shell">
